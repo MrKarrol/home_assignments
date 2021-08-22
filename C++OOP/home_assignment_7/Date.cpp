f03a4243749033e196815b90ae03270f5691b1ab
KrisKaroll <g.mishell@gmail.com>
2021-08-22 18:37:39 +0700

    ha7 oop: fixed date correction

--- C++OOP/home_assignment_7/Date.cpp
+++ C++OOP/home_assignment_7/Date.cpp
@@ -1,11 +1,18 @@
 #include "Date.h"
 
+#include <exception>
+
 
 Date::Date(int day, int month, int year)
 	: m_day(day)
 	, m_month(month)
 	, m_year(year)
-{}
+{
+	if (m_day < 0 or m_day > 31)
+		throw std::invalid_argument("day must be within [0, 31]");
+	else if (m_month < 0 or m_month > 12)
+		throw std::invalid_argument("month must be within [0, 12]");
+}
 
 int Date::GetDay() const noexcept
 {
